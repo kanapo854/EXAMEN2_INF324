5e69c16906c7a23416fa579c3a58a64ac5dfa229
kanapo854 <105760545+kanapo854@users.noreply.github.com>
2022-12-05 09:28:57 -0400

    Add files via upload

---
+++ Pregunta3/WindowsFormsApplication23/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace WindowsFormsApplication23
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button1.Location = new System.Drawing.Point(20, 19);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Abrir Imagen";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(126, 19);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(552, 519);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 67);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(69, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(39, 90);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(69, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(39, 113);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(69, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 70);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "R:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 90);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "G:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 115);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "B:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(39, 166);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(69, 20);
+            this.textBox4.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(39, 190);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(69, 20);
+            this.textBox5.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 166);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "X:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 190);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Y:";
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button8.Location = new System.Drawing.Point(20, 242);
+            this.button8.Margin = new System.Windows.Forms.Padding(2);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(88, 36);
+            this.button8.TabIndex = 19;
+            this.button8.Text = "Segmento 1";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button3.Location = new System.Drawing.Point(20, 282);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 40);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "Segmento 2";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button4.Location = new System.Drawing.Point(20, 326);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 38);
+            this.button4.TabIndex = 21;
+            this.button4.Text = "Segmento 3";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(689, 549);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Pregunta3/WindowsFormsApplication23/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication23
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        int x, y;
+
+        int mR = 0, mG = 0, mB = 0;
+
+        int[] segmentos = new int[9];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Todos|*.*|Archivos JPGE|*.jpg|Archivos GIF|*.gif" ;
+            openFileDialog1.FileName = "";
+            openFileDialog1.ShowDialog();
+            bmp = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = bmp;
+
+        }
+
+        //seleccionar color
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            x = e.X;
+            y = e.Y;
+            textBox4.Text = x.ToString();
+            textBox5.Text = y.ToString();
+            Color c = new Color();
+            c = bmp.GetPixel(x,y);
+            mR = 0; mG = 0; mB = 0;
+
+            for (int i = x; i < x+10; i++)
+            {
+                for (int j = y; j < y+10; j++)
+                {
+                    c = bmp.GetPixel(i, j);
+                    mR = mR + c.R;
+                    mG = mG + c.G;
+                    mB = mB + c.B;
+                }
+            }
+            mR = mR/100;
+            mG = mG/100;
+            mB = mB/100;
+            textBox1.Text = mR.ToString();
+            textBox2.Text = mG.ToString();
+            textBox3.Text = mB.ToString();
+
+        }
+
+        //pintar segmentos con el primer color
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int mRn = 0, mGn = 0, mBn = 0;
+            Bitmap bmp2 = new Bitmap(bmp.Width, bmp.Height);
+            Color c = new Color();
+            segmentos[0] = mR;
+            segmentos[1] = mG;
+            segmentos[2] = mB;
+            for (int i = 0; i < bmp.Width-10; i=i+10)
+                for (int j = 0; j < bmp.Height-10; j=j+10)
+                {
+                    for (int i2 = i; i2 < i + 10; i2++)
+                        for (int j2 = j; j2 < j + 10; j2++)
+                        {
+                            c = bmp.GetPixel(i2, j2);
+                            mRn = mRn + c.R;
+                            mGn = mGn + c.G;
+                            mBn = mBn + c.B;
+                        }
+                    
+                        mRn = mRn / 100;
+                        mGn = mGn / 100;
+                        mBn = mBn / 100;
+
+                    if (ver(segmentos[0], segmentos[1], segmentos[2], mRn, mGn, mBn))
+                    {
+                        for (int i2 = i; i2 < i + 10; i2++)
+                            for (int j2 = j; j2 < j + 10; j2++)
+                            {
+                                bmp2.SetPixel(i2, j2, Color.Yellow);
+                            }
+                    }
+                    else if (ver(segmentos[3], segmentos[4], segmentos[5], mRn, mGn, mBn))
+                    {
+                        for (int i2 = i; i2 < i + 10; i2++)
+                            for (int j2 = j; j2 < j + 10; j2++)
+                            {
+                                bmp2.SetPixel(i2, j2, Color.White);
+                            }
+                    }
+                    else if (ver(segmentos[6], segmentos[7], segmentos[8], mRn, mGn, mBn))
+                    {
+                        for (int i2 = i; i2 < i + 10; i2++)
+                            for (int j2 = j; j2 < j + 10; j2++)
+                            {
+                                bmp2.SetPixel(i2, j2, Color.LightBlue);
+                            }
+                    }
+                    else {
+                            for (int i2 = i; i2 < i + 10; i2++)
+                                for (int j2 = j; j2 < j + 10; j2++)
+                                {
+                                    c = bmp.GetPixel(i2, j2);
+                                    bmp2.SetPixel(i2, j2, Color.FromArgb(c.R,c.G,c.B));
+                                }
+                        }
+                }
+            pictureBox1.Image = bmp2;
+        }
+
+
+        //pintar segmentos con el segundo color
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            int mRn = 0, mGn = 0, mBn = 0;
+            Bitmap bmp2 = new Bitmap(bmp.Width, bmp.Height);
+            Color c = new Color();
+            segmentos[3] = mR;
+            segmentos[4] = mG;
+            segmentos[5] = mB;
+            for (int i = 0; i < bmp.Width - 10; i = i + 10)
+                for (int j = 0; j < bmp.Height - 10; j = j + 10)
+                {
+                    for (int i2 = i; i2 < i + 10; i2++)
+                        for (int j2 = j; j2 < j + 10; j2++)
+                        {
+                            c = bmp.GetPixel(i2, j2);
+                            mRn = mRn + c.R;
+                            mGn = mGn + c.G;
+                            mBn = mBn + c.B;
+                        }
+
+                    mRn = mRn / 100;
+                    mGn = mGn / 100;
+                    mBn = mBn / 100;
+
+                    if (ver(segmentos[3], segmentos[4], segmentos[5], mRn, mGn, mBn))
+                    {
+                        for (int i2 = i; i2 < i + 10; i2++)
+                            for (int j2 = j; j2 < j + 10; j2++)
+                            {
+                                bmp2.SetPixel(i2, j2, Color.White);
+                            }
+                    } else if (ver(segmentos[0], segmentos[1], segmentos[2], mRn, mGn, mBn))
+                    {
+                        for (int i2 = i; i2 < i + 10; i2++)
+                            for (int j2 = j; j2 < j + 10; j2++)
+                            {
+                                bmp2.SetPixel(i2, j2, Color.Yellow);
+                            }
+                    }
+                    else if (ver(segmentos[6], segmentos[7], segmentos[8], mRn, mGn, mBn))
+                    {
+                        for (int i2 = i; i2 < i + 10; i2++)
+                            for (int j2 = j; j2 < j + 10; j2++)
+                            {
+                                bmp2.SetPixel(i2, j2, Color.LightBlue);
+                            }
+                    }
+                    else
+                    {
+                        for (int i2 = i; i2 < i + 10; i2++)
+                            for (int j2 = j; j2 < j + 10; j2++)
+                            {
+                                c = bmp.GetPixel(i2, j2);
+                                bmp2.SetPixel(i2, j2, Color.FromArgb(c.R, c.G, c.B));
+                            }
+                    }
+                }
+            pictureBox1.Image = bmp2;
+        }
+
+        //pintar segmentos con el tercer color
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            int mRn = 0, mGn = 0, mBn = 0;
+            Bitmap bmp2 = new Bitmap(bmp.Width, bmp.Height);
+            Color c = new Color();
+            segmentos[6] = mR;
+            segmentos[7] = mG;
+            segmentos[8] = mB;
+            for (int i = 0; i < bmp.Width - 10; i = i + 10)
+                for (int j = 0; j < bmp.Height - 10; j = j + 10)
+                {
+                    for (int i2 = i; i2 < i + 10; i2++)
+                        for (int j2 = j; j2 < j + 10; j2++)
+                        {
+                            c = bmp.GetPixel(i2, j2);
+                            mRn = mRn + c.R;
+                            mGn = mGn + c.G;
+                            mBn = mBn + c.B;
+                        }
+
+                    mRn = mRn / 100;
+                    mGn = mGn / 100;
+                    mBn = mBn / 100;
+
+                    if (ver(segmentos[6], segmentos[7], segmentos[8], mRn, mGn, mBn))
+                    {
+                        for (int i2 = i; i2 < i + 10; i2++)
+                            for (int j2 = j; j2 < j + 10; j2++)
+                            {
+                                bmp2.SetPixel(i2, j2, Color.LightBlue);
+                            }
+                    }
+                    else if (ver(segmentos[0], segmentos[1], segmentos[2], mRn, mGn, mBn))
+                    {
+                        for (int i2 = i; i2 < i + 10; i2++)
+                            for (int j2 = j; j2 < j + 10; j2++)
+                            {
+                                bmp2.SetPixel(i2, j2, Color.Yellow);
+                            }
+                    }
+                    else if (ver(segmentos[3], segmentos[4], segmentos[5], mRn, mGn, mBn))
+                    {
+                        for (int i2 = i; i2 < i + 10; i2++)
+                            for (int j2 = j; j2 < j + 10; j2++)
+                            {
+                                bmp2.SetPixel(i2, j2, Color.White);
+                            }
+                    }
+                    else
+                    {
+                        for (int i2 = i; i2 < i + 10; i2++)
+                            for (int j2 = j; j2 < j + 10; j2++)
+                            {
+                                c = bmp.GetPixel(i2, j2);
+                                bmp2.SetPixel(i2, j2, Color.FromArgb(c.R, c.G, c.B));
+                            }
+                    }
+                }
+            pictureBox1.Image = bmp2;
+        }
+
+
+        public bool ver(int x, int y , int z, int mx, int my, int mz)
+        {
+            return (x - 10 <= mx) && (mx <= x + 10) &&
+                      ((y - 10 <= my) && (my <= y + 10) &&
+                      (z - 10 <= mz) && (mz <= z + 10));
+        }
+    }
+}
